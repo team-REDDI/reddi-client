1101c179e20a00dce1a4edfeef181da3195fe58e
leejin_rho <tbdpapdl@gmail.com>
2024-01-13 03:53:14 +0900

    feat: 검색창 기본 UI 구현

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,12 +3,14 @@ import { colors } from "../styles/colors";
 import { useCallback, useEffect, useState } from "react";
 import { ReactComponent as SearchIcon } from "../assets/svgs/searchIcon.svg";
 import { MarketingBoxSmall } from "./Home/MarketingBoxSmall";
+import dropdownDataMarketing from "../assets/datas/dropDownDataMarketing.json";
 
 type SearchBarProps = {
   show: boolean;
 };
 
 export const SearchBar: React.FC<SearchBarProps> = (props) => {
+  const [wordList, setWordList] = useState<string>();
   useEffect(() => {}, [props.show]);
 
   const [inputValue, setInputValue] = useState<string>("");
@@ -39,23 +41,35 @@ export const SearchBar: React.FC<SearchBarProps> = (props) => {
         </InputContainer>
       </SearchInputContainer>
       <SearchListBox show={props.show}>
-        <WordBox>
+        <WordsListBox>
           <SearchTitle>인기 검색어</SearchTitle>
-        </WordBox>
+          <WordsList>
+            {Object.values(dropdownDataMarketing)
+              .flat()
+              .map((item) => (
+                <WordBox>
+                  <Word>{item.value}</Word>
+                  <WordNum>91</WordNum>
+                </WordBox>
+              ))}
+          </WordsList>
+        </WordsListBox>
         <ReferenceBox>
           <SearchTitle>인기 마케팅 레퍼런스</SearchTitle>
-          <MarketingBoxSmall
-            lank={1}
-            imgSrc="../assets/images/exemple.png"
-            title="더 현대를 밝히는 ‘해리의 꿈의 상점’"
-            expl="유럽 어느 골목을 들어와있는 듯한 착각"
-          />
-          <MarketingBoxSmall
-            lank={2}
-            imgSrc="../assets/images/exemple.png"
-            title="더 현대를 밝히는 ‘해리의 꿈의 상점’"
-            expl="유럽 어느 골목을 들어와있는 듯한 착각"
-          />
+          <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
+            <MarketingBoxSmall
+              lank={1}
+              imgSrc="../assets/images/exemple.png"
+              title="더 현대를 밝히는 ‘해리의 꿈의 상점’"
+              expl="유럽 어느 골목을 들어와있는 듯한 착각"
+            />
+            <MarketingBoxSmall
+              lank={2}
+              imgSrc="../assets/images/exemple.png"
+              title="더 현대를 밝히는 ‘해리의 꿈의 상점’"
+              expl="유럽 어느 골목을 들어와있는 듯한 착각"
+            />
+          </div>
         </ReferenceBox>
       </SearchListBox>
     </SearchContainer>
@@ -96,6 +110,7 @@ const InputContainer = styled.form`
   border-bottom: 0.0625rem solid #d9d9d9;
   height: 2.2rem;
   width: 35.25rem;
+  padding-bottom: 0.35rem;
 `;
 
 const InputBar = styled.input`
@@ -134,11 +149,42 @@ const SearchListBox = styled.div<SearchContainerProps>`
   transition: transform 10.3s ease-in;
 `;
 
-const WordBox = styled.div`
+const WordsListBox = styled.div`
   display: flex;
   flex-direction: column;
   width: 28.5rem;
   height: fit-content;
+  margin-right: 4.25rem;
+`;
+
+const WordsList = styled.div`
+  display: flex;
+  width: 28.5rem;
+  flex-wrap: wrap;
+  gap: 1.5rem 3.12rem;
+`;
+
+const WordBox = styled.div`
+  display: flex;
+  justify-content: start;
+`;
+
+const Word = styled.div`
+  color: ${colors.black_CTA};
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 130%;
+  letter-spacing: -0.01rem;
+`;
+
+const WordNum = styled.span`
+  color: #525252;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.0075rem;
+  margin-left: 0.31rem;
 `;
 
 const ReferenceBox = styled.div`
@@ -155,4 +201,5 @@ const SearchTitle = styled.span`
   font-weight: 700;
   line-height: 130%;
   letter-spacing: -0.015rem;
+  margin-bottom: 2.44rem;
 `;
