c7989006552cb947c1f65409660b8210821d479d
leejin_rho <tbdpapdl@gmail.com>
2024-01-13 15:50:16 +0900

    #17 brandBox 수정

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,12 +1,71 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import {
+  BrandResultBox,
+  MarketingResultBox,
+  ResultBox,
+  ResultContainer,
+  ResultText,
+  ResultTitle,
+} from "../styles/searchStyle";
+import { MarketingBox } from "../components/MarketingBox";
+import { MarketingLine } from "../styles/HomeStyle";
+import { BrandBox } from "../components/BrandBox";
 
 const SearchResult = () => {
   return (
-    <>
+    <ResultContainer>
       <NavBar />
+      <ResultBox>
+        <ResultTitle>'공간마케팅' 검색 결과</ResultTitle>
+        <MarketingResultBox>
+          <ResultText>
+            ‘공간마케팅’이(가) 본문/제목에 언급된 마케팅 콘텐츠입니다. (3)
+          </ResultText>
+          <MarketingLine>
+            <MarketingBox
+              imgSrc="../assets/images/exemple.png"
+              type="PLACE"
+              title="더 현대를 밝히는 ‘해리의 꿈의 상점’"
+              expl="유럽 어느 골목을 들어와있는 듯한 착각"
+              read={727}
+            />
+            <MarketingBox
+              imgSrc="../assets/images/exemple.png"
+              type="PLACE"
+              title="신세계 백화점의 ‘MAGIC WINTER FANTASY’"
+              expl="3분을 위한 9개월의 여정"
+              read={1928}
+            />
+            <MarketingBox
+              imgSrc="../assets/images/exemple.png"
+              type="PLACE"
+              title="시몬스테라스의 ‘크리스마스 일루미네이션"
+              expl="동화 속 마을로 단장한 시몬스"
+              read={567}
+            />
+          </MarketingLine>
+        </MarketingResultBox>
+        <BrandResultBox>
+          <ResultText>
+            ‘공간마케팅’이(가) 본문/제목에 언급된 브랜드입니다. (4)
+          </ResultText>
+          <BrandBox
+            imgSrc=""
+            brandName="토스증권"
+            location="대기업, 서울"
+            tag="IT/금융"
+          />
+          <BrandBox
+            imgSrc=""
+            brandName="토스증권"
+            location="대기업, 서울"
+            tag="IT/금융"
+          />
+        </BrandResultBox>
+      </ResultBox>
       <Footer />
-    </>
+    </ResultContainer>
   );
 };
 
--- src/styles/searchStyle.ts
+++ src/styles/searchStyle.ts
@@ -1,1 +1,50 @@
 import styled from "styled-components";
+import { colors } from "./colors";
+
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ResultBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 64rem;
+  gap: 6.25rem;
+  margin-top: 6.25rem;
+`;
+
+export const ResultTitle = styled.span`
+  color: ${colors.black_CTA};
+  font-size: 2.25rem;
+  font-weight: 700;
+  line-height: 130%;
+  letter-spacing: -0.0225rem;
+`;
+
+export const MarketingResultBox = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+export const ResultText = styled.span`
+  color: ${colors.red};
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 130%;
+  letter-spacing: -0.015rem;
+`;
+
+export const BrandResultBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BrandsBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
