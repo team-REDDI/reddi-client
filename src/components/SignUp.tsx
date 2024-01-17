59a79c597211cc0e79fcc74c5d2f961ff0cbcd30
dhsin98 <shinjay417@gmail.com>
2024-01-17 14:59:14 +0900

    #30 feat: SignUp 이미지, 텍스트 슬라이딩 구현

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 import { colors } from "../styles/colors";
 import { useEffect, useState } from "react";
-import { ReactComponent as SignUpPeople } from "../assets/svgs/signupPeopleImage.svg";
+import React from "react";
+import { ReactComponent as SignUpPeople1 } from "../assets/svgs/signupPeopleImage.svg";
+import { ReactComponent as SignUpPeople2 } from "../assets/svgs/signupPeopleImage2.svg";
+import { ReactComponent as SignUpPeople3 } from "../assets/svgs/signupPeopleImage3.svg";
+import { ReactComponent as LeftSlider } from "../assets/svgs/leftSlider.svg";
+import { ReactComponent as RightSlider } from "../assets/svgs/rightSlider.svg";
 import { ReactComponent as GoogleLogo } from "../assets/svgs/googleLogo.svg";
 import { ReactComponent as CloseIcon } from "../assets/svgs/closeButton.svg";
 import { ReactComponent as RLogoIcon } from "../assets/svgs/RIcon.svg";
@@ -28,6 +33,36 @@ const SignUp: React.FC<SignUpProps & { onSwitchLogin: () => void }> = (
     onSwitchLogin();
   };
 
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideData = [
+    {
+      image: SignUpPeople1,
+      bigText: "수많은 브랜딩 레퍼런스 중 내게 딱 맞는 건?",
+      smallText:
+        "레디는 당신에게 딱 맞는 레퍼런스와 AI를 통한 브랜딩까지 제안합니다.",
+    },
+    {
+      image: SignUpPeople2,
+      bigText: "브랜드의 여정을 기록하다",
+      smallText: "레디는 브랜드의 시작과 지금,미래까지의 과정을 기록합니다.",
+    },
+    {
+      image: SignUpPeople3,
+      bigText: "브랜드의 성장을 돕다",
+      smallText:
+        "레디는 아카이빙을 넘어, AI를 통해 브랜드의 핵심과 미래에 대한 영감을 제공합니다.",
+    },
+  ];
+  const goToNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slideData.length);
+  };
+
+  const goToPreviousSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + slideData.length) % slideData.length,
+    );
+  };
+
   useEffect(() => {}, [props.show]);
 
   return (
@@ -38,17 +73,25 @@ const SignUp: React.FC<SignUpProps & { onSwitchLogin: () => void }> = (
           <CloseButton onClick={closeSignUp}>
             <CloseIcon />
           </CloseButton>
-          <InfoSection>
-            <SignUpPeople />
-            <InfoBigText>
-              수많은 브랜딩 레퍼런스 중 내게 딱 맞는 건?{" "}
-            </InfoBigText>
-            <InfoSmallText>
-              레디는 당신에게 딱 맞는 레퍼런스와 AI를 통한 브랜딩까지
-              제안합니다.
-            </InfoSmallText>
-          </InfoSection>
-          <InfoSection>
+          <InfoSection1>
+            {React.createElement(slideData[currentSlide].image)}
+            <InfoBigText>{slideData[currentSlide].bigText}</InfoBigText>
+            <InfoSmallText>{slideData[currentSlide].smallText}</InfoSmallText>
+            <Sliders>
+              <LeftSliderIcon onClick={goToPreviousSlide} />
+              <Dots>
+                {slideData.map((_, index) => (
+                  <Dot
+                    key={index}
+                    isSelected={currentSlide === index}
+                    onClick={() => setCurrentSlide(index)}
+                  />
+                ))}
+              </Dots>
+              <RightSliderIcon onClick={goToNextSlide} />
+            </Sliders>
+          </InfoSection1>
+          <InfoSection2>
             <SignUpBigText>1초만에 레디 시작하기</SignUpBigText>
             <SignUpSmallText>
               아이디, 비밀번호, 이름, 휴대번호를 입력하지 않고도 간편하게 시작할
@@ -64,7 +107,7 @@ const SignUp: React.FC<SignUpProps & { onSwitchLogin: () => void }> = (
                 간편 로그인하기
               </LoginOpen>
             </LoginText>
-          </InfoSection>
+          </InfoSection2>
         </SignUpWrapper>
       </SignUpContainer>
     </>
@@ -109,18 +152,18 @@ const CloseButton = styled.button`
 `;
 const FixedRLogoIcon = styled(RLogoIcon)`
   position: absolute;
-  top: 2.12rem;
-  left: 2.44rem; // 피그마랑 다르게 수정했음
+  top: 3.62rem;
+  left: 2.94rem;
 `;
-
-const InfoSection = styled.div`
+const InfoSection1 = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
 `;
+
 const InfoBigText = styled.div`
   width: 15.3125rem;
-  height: 3.875rem;
+  height: fit-content;
   text-align: center;
   font-size: 1.5rem;
   font-style: normal;
@@ -131,7 +174,7 @@ const InfoBigText = styled.div`
   margin-bottom: 0.25rem;
 `;
 const InfoSmallText = styled.div`
-  width: 13.125rem;
+  width: 16.375rem;
   height: 2.5rem;
   color: var(--gray-text, var(--gray, #595959));
   text-align: center;
@@ -139,6 +182,41 @@ const InfoSmallText = styled.div`
   font-style: normal;
   font-weight: 500;
   line-height: 140%;
+`;
+
+const Sliders = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.56rem;
+  margin-top: 1.13rem;
+`;
+const LeftSliderIcon = styled(LeftSlider)`
+  cursor: pointer;
+`;
+const RightSliderIcon = styled(RightSlider)`
+  cursor: pointer;
+`;
+
+const Dots = styled.div`
+  display: flex;
+  gap: 0.56rem;
+`;
+
+const Dot = styled.div<{ isSelected: boolean }>`
+  width: 0.4375rem;
+  height: 0.4375rem;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  background-color: ${(props) => (props.isSelected ? "#ccc" : "white")};
+  display: inline-block;
+
+  cursor: pointer;
+`;
+
+const InfoSection2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const SignUpBigText = styled.div`
