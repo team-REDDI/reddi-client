e5457037017a7060111f7f76dae6aef17cc1ebc1
dhsin98 <shinjay417@gmail.com>
2024-01-23 15:52:43 +0900

    #40 chore: marketing dummybox 태그 수정

--- src/pages/Marketing.tsx
+++ src/pages/Marketing.tsx
@@ -17,7 +17,7 @@ const dummyMarketingBoxes = [
     title: "더 현대를 밝히는 ‘해리의 꿈의 상점’",
     expl: "유럽 어느 골목을 들어와있는 듯한 착각",
     read: 727,
-    categories: ["부티크", "팝업스토어", "콘텐츠 마케팅"],
+    categories: ["F&B", "팝업스토어", "여성"],
   },
   {
     imgSrc: "../assets/images/exemple.png",
@@ -25,7 +25,7 @@ const dummyMarketingBoxes = [
     title: "신세계 백화점의 ‘MAGIC WINTER FANTASY’",
     expl: "3분을 위한 9개월의 여정",
     read: 567,
-    categories: ["부티크", "팝업스토어", "10대"],
+    categories: ["부티크", "팝업스토어", "직장인"],
   },
   {
     imgSrc: "../assets/images/exemple.png",
@@ -33,7 +33,7 @@ const dummyMarketingBoxes = [
     title: "시몬스테라스의 ‘크리스마스 일루미네이션",
     expl: "동화 속 마을로 단장한 시몬스",
     read: 1218,
-    categories: ["부티크", "팝업스토어", "콘셉트마케팅"],
+    categories: ["부티크", "이벤트 마케팅", "사회초년생"],
   },
   {
     imgSrc: "../assets/images/exemple.png",
@@ -41,7 +41,7 @@ const dummyMarketingBoxes = [
     title: "시몬스테라스의 ‘크리스마스 일루미네이션",
     expl: "동화 속 마을로 단장한 시몬스",
     read: 1218,
-    categories: ["부티크", "팝업스토어", "콘셉트마케팅"],
+    categories: ["패션", "콜라보 마케팅", "콘텐츠 마케팅"],
   },
 ];
 
