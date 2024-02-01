65e73c4408512afb2220fd07e4d251d746bbaa01
leejin_rho <tbdpapdl@gmail.com>
2024-02-01 16:30:04 +0900

    #102 fix: mypage ui 수정

--- src/styles/mypageStyle.ts
+++ src/styles/mypageStyle.ts
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { colors } from './colors';
+import styled from "styled-components";
+import { colors } from "./colors";
 
 export const Container = styled.div`
   display: flex;
